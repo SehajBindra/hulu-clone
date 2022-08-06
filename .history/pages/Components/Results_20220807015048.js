ed49bd9e249257865bd687e35dfa9c14da06b1d9
Sehaj bindra <sehajbindra1234@gmail.com>
2022-08-07 01:54:46 +0530

    thumbnail

---
+++ .history/pages/Components/Results_20220807015048.js
@@ -0,0 +1,13 @@
+import FlipMove from "react-flip-move";
+
+function Results({ results }) {
+  return (
+    <FlipMove className="px-8  my-10 sm:grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 3xl: flex justify-center flex-wrap">
+      {results.map((result) => (
+        <Thumbnail key={result.id} result={result} />
+      ))}
+    </FlipMove>
+  );
+}
+
+export default Results;
