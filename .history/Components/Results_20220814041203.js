54d447a05fee9b3014f585cfb7073544f12e81df
Sehaj bindra <sehajbindra1234@gmail.com>
2022-08-14 04:16:04 +0530

    Results

--- .history/Components/Results_20220814041203.js
+++ .history/Components/Results_20220814041203.js
@@ -1,10 +1,11 @@
 import Thumnail from "./Thumnail";
+import FlipMove from "react-flip-move";
 
 function Results({ results }) {
   return (
     <div className="px-8  my-10 sm:grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 3xl: flex justify-center flex-wrap">
-      {results?.map((result) => (
-        <Thumnail key={result.id} result={result} />
+      {results.map((result) => (
+        <Thumnail key={result.id} />
       ))}
     </div>
   );
--- .history/Components/Results_20220814041250.js
+++ .history/Components/Results_20220814041250.js
@@ -3,7 +3,7 @@ import Thumnail from "./Thumnail";
 function Results({ results }) {
   return (
     <div className="px-8  my-10 sm:grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 3xl: flex justify-center flex-wrap">
-      {results?.map((result) => (
+      {results.map((result) => (
         <Thumnail key={result.id} result={result} />
       ))}
     </div>
